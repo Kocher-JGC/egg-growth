b08bf5bf65e57c56d8d3b95fbe4d30c1ee129670
Kocher-JGC <879515820@qq.com>
2018-12-27 09:49:53 +0800

    修改书写方式

--- app/controller/mongo/user.js
+++ app/controller/mongo/user.js
@@ -1,6 +1,16 @@
 'use strict';
 
-exports.index = function* (ctx) {
-  console.log(ctx.model.Mongod.User.find({}));
-  ctx.body = yield ctx.model.Mongod.User.find({});
-};
+// exports.index = function* (ctx) {
+//   console.log(ctx.model.Mongod.User.find({}));
+//   ctx.body = yield ctx.model.Mongod.User.find({});
+// };
+
+const Controller = require('egg').Controller;
+
+class UserController extends Controller {
+  async index() {
+    this.ctx.body = await this.ctx.model.Mongod.User.find({});
+  }
+}
+
+module.exports = UserController;
